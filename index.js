a2bb20c03884058da38581122bbaa12eeb2bc14e
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-13 01:07:54 -0600

    commit tree a0955018208fc0fdffa19860a1416c7471e7ec86

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4'
+dodger.style.bottom = '0px'
+dodger.style.left = '0px'
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowLeft'){
+        const leftNumbers = dodger.style.left.replace('px', '')
+        const left = parseInt(leftNumbers, 10)
+        dodger.style.left = `${left - 1}px`
+    }
+})
+
+moveDodgerLeft()
+moveDodgerRight()
